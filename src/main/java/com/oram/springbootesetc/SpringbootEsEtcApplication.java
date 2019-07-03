fc22486b775ee1995f839ac825053f0b0123b07a
gaokai <oraman@163.com>
2019-07-03 10:08:11 +0800

    springboot initializr

---
+++ src/main/java/com/oram/springbootesetc/SpringbootEsEtcApplication.java
@@ -0,0 +1,13 @@
+package com.oram.springbootesetc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEsEtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEsEtcApplication.class, args);
+	}
+
+}
